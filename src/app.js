91231d4a794d7be38fd955a6815a17ddb204e883
Ararat <melkumyan2077@gmail.com>
2023-06-15 13:30:04 +0300

    update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/style.css";
 import taskFieldTemplate from "./templates/taskField.html";
+import taskFieldFooterTemplate from "./templates/taskFieldFooter.html";
 import noAccessTemplate from "./templates/noAccess.html";
 import { User } from "./models/User";
 import { generateTestUser } from "./utils";
@@ -12,7 +13,15 @@ export const appState = new State();
 const loginForm = document.querySelector("#app-login-form");
 
 generateTestUser(User);
+// debug start
+let fieldHTMLContentMain =taskFieldTemplate
 
+document.querySelector(".main").innerHTML = fieldHTMLContentMain;
+
+let fieldHTMLContentFooter = taskFieldFooterTemplate
+
+document.querySelector(".footer").innerHTML = fieldHTMLContentFooter;
+// debug end
 loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const formData = new FormData(loginForm);
